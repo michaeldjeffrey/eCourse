bac97cf86b3cd9b915bdc5e7ab58d5a830a2a9a4
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-09-15 21:22:49 -0700

    add update to lessons

--- lesson/urls.py
+++ lesson/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', ListLessonView.as_view(), name='list'),
     url(r'^new/$', CreateLessonView.as_view(), name='new'),
     url(r'^(?P<pk>\d+)/$', LessonView.as_view(), name='view'),
+    url(r'^(?P<pk>\d+)/edit$', UpdateLessonView.as_view(), name='edit'),
     url(r'^delete/(?P<pk>\d+)/$', DeleteLessonView.as_view(), name='delete'),
 )
